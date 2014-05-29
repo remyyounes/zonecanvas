c6fd7a480e211f86a9d69371b813a4d3dd8151b3
remyyounes <ryounes@gmail.com>
2014-05-28 17:13:51 -0700

    delete/cleaning

--- public/js/canvasDemo.js
+++ public/js/canvasDemo.js
@@ -10,7 +10,6 @@
 
   CanvasDemo.prototype = {
     init: function(){
-      this.attachHandlers();
       if(this.imageUrl) this.loadImage(this.imageUrl);
     },
     addCanvas: function(params){
@@ -21,48 +20,30 @@
         viewZone: params.viewZone
       }));
       this.$el.append(canvas);
-    },
-    attachHandlers: function(){
-
-    },
+    }
     showGrid: function(){
 
-      canvasDemo.addCanvas({
-        image: canvasDemo.image
-      });
-
-      canvasDemo.addCanvas({
-        image: canvasDemo.image,
-        viewZone: {x: 0, y: 0, width: 200, height:300 }
-      });
-
-      canvasDemo.addCanvas({
-        image: canvasDemo.image,
-        viewZone: {x: 200, y: 300, width: 400, height:600 }
-      });
-
-      // canvasDemo.addCanvas({
-      //   image: canvasDemo.image,
-      //   viewZone: {x: 400, y: 0, width: 200, height:300 }
-      // });
-
-      canvasDemo.addCanvas({
-        image: canvasDemo.image,
-        viewZone: {x: 600, y: 900, width: 200, height:300 }
-      });
-
-
+      var imgWidth = canvasDemo.image.width;
+      var imgHeight = canvasDemo.image.height;
+      var subdivisions = 4;
+      var zone = {};
+      for(var i = 1; i <= subdivisions; i*=2 ){
+        zone.width = imgWidth/i;
+        zone.height = imgHeight/i;
+        zone.x = imgWidth - zone.width;
+        zone.y = imgHeight - zone.height;
+        canvasDemo.addCanvas({ image: canvasDemo.image, viewZone: zone });
+      }
     },
     loadImage: function(file, zoom){
       var canvasDemo = this;
       zoom = zoom || 1;
-
       canvasDemo.image = new Image();
       canvasDemo.image.src = this.imageUrl;
       canvasDemo.image.onerror = function (e){ alert("Image loading error. " + e); };
       canvasDemo.image.onload = function(){ canvasDemo.showGrid(); };
     }
-  }
+  };
 
   window.CanvasDemo = CanvasDemo;
 
