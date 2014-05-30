7343320d020598dfda48b140003c83948ccd8381
remyyounes <ryounes@gmail.com>
2014-05-30 16:47:31 -0700

    refactoring in modules

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ var path = require('path');
 var fs = require('fs');
 var gm = require('gm');
 var multer  = require('multer');
-var exec = require('child_process').exec;
-var http = require('http');
 var imgProc = require('./lib/image_processing');
-var rest = require('restler');
+var classifier = require('./lib/classifier');
+
+
 
 
 // var imagePath = '/documents/gruid-test-guide.png';
@@ -19,63 +19,10 @@ var express = require('express'),
   fs_readFile = q.denodeify(fs.readFile),
   fs_writeFile = q.denodeify(fs.writeFile);
 
-var ocrServer = "192.168.33.42";
-var ocrPath = "/external_api/drawing_sets";
 // "name=test_drawing" -F "drawing_pdf=@package_bde_elan.pdf"  >
 
 
-
-var testDjango = function(pdfPath){
-  var deferred = q.defer();
-  var options = {
-    host: ocrServer,
-    path: ocrPath,
-    port: 4567,
-    method: "GET"
-  };
-
-  var req = http.request(options, function(res) {
-  });
-
-  req.on('error', function(err) {
-    throw err;
-  });
-
-  req.end();
-  fs.createReadStream(pdfPath).pipe(req);
-
-  return deferred.promise;
-}
-
-var djangoOcr = function(pdfPath, documentName){
-  var deferred = q.defer();
-  var endPoint = "http://" + ocrServer + ":4567" + ocrPath;
-
-  fs.stat(pdfPath, function (err, stats) {
-
-    rest.post( endPoint,{
-      multipart: true,
-      data: {
-        'name': 'test.pdf',
-        'drawing_pdf': rest.file(pdfPath, null, stats.size, null, 'application/pdf')
-      }
-    })
-    .on('complete', function(data) {
-      console.log('data', data);
-      deferred.resolve();
-    });
-
-  });
-
-
-  return deferred.promise;
-};
-
-
-app.use(bodyParser({
-  // keepExtensions:true,
-  // uploadDir:path.join(__dirname,'/public/tmp')
-}));
+app.use(bodyParser());
 app.use(multer({ dest: './public/uploads'}))
 app.use(express.static(__dirname + '/public'));
 
@@ -94,48 +41,21 @@ app.post('/ocrdata', function(req, res){
 
 app.post('/pdf', function(req, res){
 
-  var documentId = 2
+  var documentId = 3;
   var file = req.files.file;
   var pdfPathRelative = file.path;
-  var outputPathRelative = path.join('tmp/', documentId.toString(), '/');
+  var outputPathRelative = path.join('tmp');
   var pdfPathFull = path.join(__dirname, pdfPathRelative);
-  var outputPathFull = path.join(__dirname, '/public/', outputPathRelative);
-  !fs.existsSync(outputPathFull) && fs.mkdirSync(outputPathFull);
-  var filenameTemplate = "page";
-  var cmd = 'pdftocairo -png -r 300 ' + pdfPathFull + " "+ outputPathFull + filenameTemplate;
+  var pngPathOutput = path.join(__dirname, 'public', outputPathRelative);
 
-  djangoOcr(pdfPathFull, "test_drawing");
-  // testDjango(pdfPathFull);
+  // classifier.djangoOcr(pdfPathFull, "test_drawing");
+  console.log(pdfPathFull, pngPathOutput, documentId);
 
-  exec(cmd, function (error, stdout, stderr) {
-    var dir = fs.readdirSync(outputPathFull);
-    var pages = [];
-    var page = 1;
-    var series = function(item, page){
-      if(item && item.slice(-4) !== ".png"){
-        series(dir.shift(), page);
-      } else if(item) {
-        var filePath = outputPathFull + item;
-        imgProc.getOrientation(filePath).then(function(data){
-          var deferred = q.defer();
-          var angle = data.result;
-          return imgProc.rotateImage(filePath, angle);
-        }).then(function(data){
-          pages.push({
-            documentId: documentId,
-            page: page,
-            url: outputPathRelative + filenameTemplate + "-" + page + ".png"
-          });
-          page++;
-          series(dir.shift(), page);
-        });
-      }else{
-        console.log("responding with pages:", pages)
-        res.json(pages);
-      }
-    };
-    series(dir.shift(), page);
+  imgProc.pdfToPng(pdfPathFull, pngPathOutput, documentId).then(function(pages){
+    console.log("responding with pages:", pages);
+    res.json(pages);
   });
+
 });
 
 app.post('/ocr', function(req, res){
